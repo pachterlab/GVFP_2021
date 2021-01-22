465d0141e74ac43bbe2469976966629f14fe3212
Meichen Fang <fang@Meichens-MacBook-Pro.local>
2021-01-22 11:57:09 -0800

    temp

--- Generate_CIR_data_args.py
+++ Generate_CIR_data_args.py
@@ -36,14 +36,14 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--outdir", default="output", type=str, action='store', help='output dir')
     args = parser.parse_args()
 
-    paras = pd.read_csv('data/param_vals.txt', sep='\t', index_col=0, header = 0)
+    paras = pd.read_csv('data/gg_201206_cir_param_vals_2.txt', sep='\t', index_col=0, header = 0)
     names = ('1_intrinsic','2_extrinsic','3_poisson')#,'4_fastnoise','5_intermed','6_intermed')
     names = [os.path.join(args.outdir,'CIR_'+i_+'.mat') for i_ in names]
     
     if not os.path.isdir(args.outdir):
         os.mkdir(args.outdir)
     #%%        
-    for i in range(1,4):
+    for i in range(1,7):
         trun = time.time()
         # load parameters
         beta = paras.at[i,'beta'] 
